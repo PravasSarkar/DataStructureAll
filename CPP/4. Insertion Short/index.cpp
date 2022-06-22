1b04febd6440203bdb7639a351ba4937418bb370
PravasSarkar <83358846+PravasSarkar@users.noreply.github.com>
2022-06-23 01:13:00 +0600

    push

--- CPP/4. Insertion Short/index.cpp
+++ CPP/4. Insertion Short/index.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 int main(){
-    int n,i,j,k,m,temp,arr[] = { 5, 1, 4, 2, 8 ,10 , 76, 56, 43, 12 };
+    int n,i,j,k,m,temp,arr[] = { 5, 1, 4, 2, 8 ,10};
 
     n = sizeof(arr)/sizeof(arr[0]);
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n-i;j++){
-            if(arr[j-1]>arr[j]){
-                temp = arr[j-1];
-                arr[j-1]=arr[j];
-                arr[j]=temp;
+    for(i=1;i<n;i++){
+            temp = arr[i];
+            j=i-1;
+            while(j>=0 && arr[j] > temp){
+                arr[j+1]=arr[j];
+                   j--;
             }
-        }
+
+            arr[j+1]=temp;
     }
 
     for(k=0;k<n;k++){
